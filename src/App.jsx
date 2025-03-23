2a8616194d964633c3aa2c7968680670846d9dba
AndriusDac <jaad75@gmail.com>
2025-03-24 00:29:19 +0200

    fix  tests

--- src/App.jsx
+++ src/App.jsx
@@ -43,7 +43,7 @@ const App = () => {
     </div>
   );
 };
-const calculateWinner = (board) => {
+export const calculateWinner = (board) => {
   const lines = [
     [0, 1, 2], [3, 4, 5], [6, 7, 8],
     [0, 3, 6], [1, 4, 7], [2, 5, 8],
